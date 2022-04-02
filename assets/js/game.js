6d93cc0a2d1672c42523c98415ded1352c909129
vdemel <vickijdemel@gmail.com>
2022-04-02 16:53:56 -0500

    updated shop option for numeric values

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,6 +28,7 @@ if (promptFight === "" || promptFight === null) {
     }
   }
 }
+}
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
@@ -158,33 +159,27 @@ var endGame = function() {
 // go to shop between battles function
 var shop = function() {
   // ask player what they'd like to do
+  var shopOptionPrompt = parseInt(shopOptionPrompt);
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
-
-  // use switch case to carry out action
-  switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
-      playerInfo.refillHealth();
-      break;
-    case 'UPGRADE':
-    case 'upgrade':
-      playerInfo:upgradeAttack();
-      break;
-    case 'LEAVE':
-    case 'leave':
-      window.alert('Leaving the store.');
-
-      // do nothing, so function will end
-      break;
-    default:
-      window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
-      shop();
-      break;
-  }
+ // use switch case to carry out action
+ 
+ switch (shopOptionPrompt) {
+  case 1:
+    playerInfo.refillHealth();
+    break;
+  case 2:
+    playerInfo.upgradeAttack();
+    break;
+  case 3:
+    window.alert("Leaving the store.");
+    break;
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+    shop();
+    break;
+} 
 };
 
 // function to generate a random numeric value
